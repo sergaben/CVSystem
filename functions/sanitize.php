06b0439b4c54e2ad2024bbc5dc1055421befdc40
Sergio <gabense@hotmail.com>
2017-09-12 01:58:29 +0100

    No needed file

--- functions/sanitize.php
+++
@@ -1,6 +0,0 @@
-<?php
-// the function below will delete html tags and convert double and single quotes into readable text so any strange input is handle properly
-function escape($string){
-        
-    return htmlspecialchars($string,ENT_QUOTES,'UTF-8');
-}
